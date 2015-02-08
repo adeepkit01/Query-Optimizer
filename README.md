91596312cedf6ae4a7d32d6efe882eb0c66f4915
Tushar Makkar <tusharmakkar08@gmail.com>
2015-02-08 17:27:59 +0530

    Location and Interest added

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ Query format will be as follows : <br>
 	// Date format would be same as `date +"%d-%m-%Y"`
 	location:[{
 			state:
-			city:
+			,city:
 		},{}]
 	interest:[{
 			broadDomain:
-			specificDomain:[specifcDomain1,specificDomain2]
+			,specificDomain:
 			},{}]	
 	affilifation:[]
 	lowestRank:
 	highestRank: 
 }
 ```
-For all those places where nothing is there just add `null` to it. <br>
-Each query object will be of form <br> `userId:[query1,query2,..]`
+For all those places where nothing is there just add `null` or `None` (in python) to it. <br>
+Each query object will be of form <br> `userId:[query1,query2,..]`<br>
 and the QueryOptimizer object will take an array of query objects as<br>
 input and return <br>
-`userId1:[result1,result2,..],userId2:[result1,result2,..]`
+`userId1:[result1,result2,..],userId2:[result1,result2,..]`<br>
--- testcases.py
+++ testcases.py
@@ -16,12 +16,12 @@ def generateTestCases(numberOfUsers, minNumberOfQueries, maxNumberOfQueries):
     Returns:
         outputQuery : JSON object
     '''
-    ## Assuming userId is numerical of 6 digits.
+    ## Assuming userId is maximum number of 7 digits.
     outputQuery = []
     for user in xrange(numberOfUsers):
         tempQuery = {}
  
-        userId = random.randint(1, 1000000)
+        userId = random.randint(1, 10000000)
  
         tempQuery["userId"] = userId
         beginDay = randint(1, 27)
@@ -48,7 +48,41 @@ def generateTestCases(numberOfUsers, minNumberOfQueries, maxNumberOfQueries):
         tempQuery["highestRank"] = random.uniform(
                         tempQuery["lowestRank"] + 0.001, 5)
                     
-        ## Location and Interest Remaining
+        location = []
+        states = [Andhra Pradesh, Arunachal Pradesh, Assam, Bihar, Chhatisgarh, Goa,
+        Gujarat, Haryana , Himachal Pradesh, Jammu & Kashmir, Jharkhand, Karnataka,
+        Kerala, Madhya Pradesh, Maharashtra, Manipur, Meghalaya, Mizoram, Nagaland,
+        Orissa, Punjab, Rajasthan, Sikkim, Tamil Nadu, Tripura, Uttar Pradesh,
+        Uttaranchal, West Bengal]
+        cities = []
+        numberOfLocations = random.randInt(1,28)
+        for states in xrange(numberOfLocations):
+            location.append({"state":states[random.randint(0,28)],
+                            "city": None })
+        tempQuery["location"] = location
+        
+        interest = []
+        broadDomain = ["CSE", "ME", "Meta", "Mining", "EE", "ECE", "IT"]
+        mapSpec = {}
+        mapSpec["specificDomainCSE"] = ["Networks", "AI", "ML", "SE", "IS"]
+        mapSpec["specificDomainME"] = ["Robotics", "Workshop", "Lathe"]
+        mapSpec["specificDomainMeta"] = ["Soil", "Temperature" , "Rock"]
+        mapSpec["specificDomainMining"] = ["Soil", "Temperature"]
+        mapSpec["specificDomainEE"] = ["Signal", "Image" , "Transistor", "Power"]
+        mapSpec["specificDomainECE"] = ["Signal", "Image" , "Transistor"]
+        mapSpec["specificDomainIT"] = ["Networks", "AI", "ML", "SE", "IS"]
+        numberOfInterest = random.randint(1,5)
+        for domain in xrange(numberOfInterest):
+            interestSpec = random.randint(0,6)
+            numberOfSpec = random.randint(1,3)
+            for specification in xrange(numberOfSpec):
+                interest.append({"broadDomain": broadDomain[interestSpec]
+                , "specificDomain": 
+                mapSpec[str(specificDomain)+str(broadDomain[interestSpec])]
+                [random.randint(0,2)]})
+        tempQuery["interest"] = interest
+        
+        outputQuery.append(tempQuery)
         
 if __name__ == '__main__':
     generateTestCases(3, 1, 4)
