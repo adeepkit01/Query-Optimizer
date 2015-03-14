055974d7b25a41aad85a4c8111f3bfb154ea89f1
Tushar Makkar <tusharmakkar08@gmail.com>
2015-03-15 01:48:00 +0530

    Unnormalized user rating added

--- Rating/rating.py
+++ Rating/rating.py
@@ -23,31 +23,59 @@ def ratingGen(flagship, affiliation, featured,
     indexing : Bool variable whether it is dblp indexed or not 
     numberOfPrev : Number of previous conferences under the same hood 
                     integer value
-    userExp : None or float [0-1 (both included)] depending whether it 
+    userExp : None or float [1-2 (both included)] depending whether it 
             is an old conference or new
     
     Output : A rating of the conference
     '''
     netRank = 0.5
     if flagship == 1 :
-        netRank += 0.5
+        netRank += 0.2
     if affiliation == 1:
-        netRank += 0.5
+        netRank += 0.3
     netRank += featured
     if numberOfPrev > 0 and numberOfPrev < 5:
-        netRank += 0.2
-    elif numberOfPrev < 10 :
-        netRank += 0.35
-    elif numberOfPrev > 10: 
-        netRank += 0.5
+        netRank += 0.15
+    elif numberOfPrev > 5 :
+        netRank += 0.3
     if qualityCollege == 1 :
-        netRank += 0.5
+        netRank += 0.3
     if qualityCollege == 2 :
-        netRank += 0.2
+        netRank += 0.15
     if indexing == 1: 
-        netRank += 0.5
+        netRank += 0.4
     netRank += userExp
     return netRank
+    
+def userQuotient(hospitalityLi, educationalQualityLi, connectivityLi):
+    '''
+    Input : 
+        hospitality = list of feedback between 1 to 5 (integers)
+        educationalQuality = list of feedback between 1 to 5 (integers)
+        connectivity = list of feedback between 1 to 5 (integers)
+    Output : 0-2 returns the user quotient (unnormalized)
+    '''
+    hospitality = sum(hospitalityLi)/float(len(hospitalityLi))
+    educationalQuality = sum(educationalQualityLi)/float(len(educationalQualityLi))
+    connectivity = sum(connectivityLi)/float(len(connectivityLi))
+    userQuot = 0
+    if hospitality >= 2 and hospitality < 4:
+        userQuot += 0.3
+    elif hospitality >= 4:
+        userQuot += 0.6
+    if educationalQuality > 2 and educationalQuality < 4:
+        userQuot += 0.3
+    elif educationalQuality >= 4:
+        userQuot += 0.6
+    if connectivity > 2 and connectivity < 4:
+        userQuot += 0.3
+    elif connectivity >= 4:
+        userQuot += 0.6
+    if connectivity == 5 or educationalQuality == 5 or hospitality == 5:
+        userQuot += 0.1
+    if connectivity == 5 and educationalQuality == 5 and hospitality == 5:
+        userQuot += 0.1
+    return userQuot
     
 def sponsorQuotient(cashConfId):
     '''
@@ -67,4 +95,5 @@ def sponsorQuotient(cashConfId):
         
 if __name__ == '__main__':
     print sponsorQuotient({1:10, 2:30, 3:5, 4:2, 5:25, 6:45, 7:32})
-    print ratingGen(1,1,1,1,1,100,1)
+    print ratingGen(1,1,1,1,1,100,2)
+    print userQuotient([2,3,4],[5,5,5],[3,1,3])
