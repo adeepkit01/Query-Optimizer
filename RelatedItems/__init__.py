d676eb7715eb5e3ef70d9ebd4aa0a7a0615df955
Tushar Makkar <tusharmakkar08@gmail.com>
2015-03-22 01:00:01 +0530

    Related Work Files made

---
+++ RelatedItems/__init__.py
@@ -0,0 +1,5 @@
+'''
+Init File
+Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
+Date: 22.03.2015
+'''
---
+++ RelatedItems/related.py
@@ -0,0 +1,7 @@
+'''
+Code for optimizing queries
+Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
+Date: 22.03.2015
+'''
+
+if __name__ == '__main__':
