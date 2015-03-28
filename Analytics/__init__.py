e8fb8704e205a884f8d7ad79cdff8cc0e57c2fd7
Tushar Makkar <tusharmakkar08@gmail.com>
2015-03-28 12:05:29 +0530

    Ananlytics folder and files made

---
+++ Analytics/__init__.py
@@ -0,0 +1,5 @@
+'''
+Init File
+Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
+Date: 28.03.2015
+'''
---
+++ Analytics/analytics.py
@@ -0,0 +1,9 @@
+'''
+Analytics Generation Email
+Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
+Date: 28.03.2015
+'''
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    
